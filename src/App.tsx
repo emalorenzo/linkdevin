66fb35e8957f6f09794ac4bcab7ef59529d54527
Ema Lorenzo <aemalorenzo@gmail.com>
2021-11-30 13:51:21 -0300

    basic app with messaging working

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,52 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+// import logo from "./logo.svg";
+import { DOMMessage, DOMMessageResponse } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tab, setTab] = useState<chrome.tabs.Tab | null>(null);
+  const [response, setResponse] = useState<DOMMessageResponse | null>(null);
+
+  useEffect(() => {
+    const getTab = async () => {
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+
+      setTab(tab);
+    };
+    getTab();
+  }, []);
+
+  useEffect(() => {
+    window.console.log("");
+    if (tab) {
+      chrome.tabs.sendMessage(
+        // Current tab ID
+        tab.id || 0,
+
+        // Message type
+        { type: "GET_DOM" } as DOMMessage,
+
+        // Callback executed when the content script sends a response
+        (response: DOMMessageResponse) => {
+          setResponse(response);
+          console.log("frula", response);
+        }
+      );
+    }
+  }, [tab]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates asadasda.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container">
+      <p>Mensajes pendientes de responder</p>
+      <pre>{JSON.stringify(tab, null, 2)}</pre>
+      <pre>{JSON.stringify(response, null, 2)}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/background.ts
@@ -0,0 +1,6 @@
+chrome.runtime.onInstalled.addListener(() => {
+  // chrome.storage.sync.set({ color });
+  // console.log('Default background color set to %cgreen', `color: ${color}`);
+
+  console.log("u been hacked bitch");
+});
